dc5df5d1b5434faa339d589fca7956b681f5761d
unknown <arprithy47@gmail.com>
2025-07-05 11:36:33 +0530

    landingpage

---
+++ Landingpage/index.js
@@ -0,0 +1,61 @@
+/*const name="Prithy"
+let age=22
+let isStudent=true
+let sen=`Hi,I am ${name} . I am ${age} years old and I am a Student:${isStudent}`
+console.log(sen)
+const age=22;
+if(age<18){
+    console.log("you are minor");
+}
+else if(age>18 && age<60){
+    console.log("you are an adult");
+}
+else{
+    console.log("you are a senior citizen");
+}
+for(let i=1;i<=age;i++){
+    console.log(i);
+}
+sayHello();
+function sayHello() {
+  console.log("Hello, Prithy!");
+}
+
+function myFunc(name){
+    console.log(`Hello,${name}! Welcome to JavaScript`);
+}
+myFunc("Prithy");
+
+const square=function(a){
+    return(a*a);
+};
+const num=6;
+console.log(`Square of ${num} is`+ square(num));
+
+const even = (a)=>{
+
+    return (a%2==0);
+  
+}
+const num1=7;
+console.log(`Is ${num1} even ?`+ even(num1));
+
+marks=[90,75,88,95,70]
+let sum=0;
+for(let i=0;i<marks.length;i++){
+    console.log(marks[i]);
+    sum=sum+marks[i];
+  
+}
+  avg=sum/marks.length;
+console.log("average"+avg);
+
+let student={
+    name:"prithy",
+    age:22,
+    rollno:41,
+    course:"JavaScript"};
+console.log(`${student.name} (Roll No:${student.rollno}) is ${student.age} years old and studying ${student.course}`)*/
+
+
+
